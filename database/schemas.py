f2a70f5618d8802e1083231ce8182713effb20f2
bishakh17 <telbdzone@gmail.com>
2023-02-23 14:01:50 +0530

    bugs

--- database/schemas.py
+++ database/schemas.py
@@ -16,33 +16,16 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-class Supplier(BaseModel):
-    name: str
-    phone: str 
-    email: str
-    password: str
-    lattitude: float
-    longitude: float
-    created_at: datetime
-
-class Product(BaseModel):
-    name: str
-    description: str
-
-
-class Order(BaseModel):
-    user_id: int
-    created_at: str 
-
-
-class PriceStock(BaseModel):
+class OrderEntity(BaseModel):
+    product_id: int
     supplier_id: int
-    product_id: int
-    price: float
-    stock: int
-
-class OrderQuantity(BaseModel):
-    order_id: int
-    product_id: int
     quantity: int
-    
+    price: int
+class OrderDetails(BaseModel):
+    id: int
+    user_id: int
+    total_price: int
+    created_at: datetime
+    order_entities: list[OrderEntity]
+
+
